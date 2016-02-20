8153ab8c6569850de206d7a545958006a3767e4e
Alex <alexoree@apache.org>
2016-02-19 21:49:04 -0500

    attempt at gradle conversion

---
+++ opus_android_demo/obj/local/armeabi/objs/OpusEncoder/de_stuttgart_hdm_opuswalkietalkie_OpusEncoder.o.d
@@ -0,0 +1,77 @@
+/Users/alex/opus_demo/opus_android_demo/obj/local/armeabi/objs/OpusEncoder/de_stuttgart_hdm_opuswalkietalkie_OpusEncoder.o: \
+ /Users/alex/opus_demo/opus_android_demo/jni/de_stuttgart_hdm_opuswalkietalkie_OpusEncoder.c \
+ /Users/alex/opus_demo/opus_android_demo/jni/de_stuttgart_hdm_opuswalkietalkie_OpusEncoder.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/alex/opus_demo/opus_android_demo/jni/../static_libs/opus/include/opus/opus.h \
+ /Users/alex/opus_demo/opus_android_demo/jni/../static_libs/opus/include/opus/opus_types.h \
+ /Users/alex/opus_demo/opus_android_demo/jni/../static_libs/opus/include/opus/opus_defines.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h
+
+/Users/alex/opus_demo/opus_android_demo/jni/de_stuttgart_hdm_opuswalkietalkie_OpusEncoder.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/alex/opus_demo/opus_android_demo/jni/../static_libs/opus/include/opus/opus.h:
+
+/Users/alex/opus_demo/opus_android_demo/jni/../static_libs/opus/include/opus/opus_types.h:
+
+/Users/alex/opus_demo/opus_android_demo/jni/../static_libs/opus/include/opus/opus_defines.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/alex/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
